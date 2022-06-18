d66b374c9b3b175d66798eabd5086f6b0f01f893
John <88310061+yeh-john@users.noreply.github.com>
2022-06-18 11:49:30 -0400

    Update wifi-dos.py

--- my-tools/wifi-dos.py
+++ my-tools/wifi-dos.py
@@ -20,19 +20,43 @@ def printLogo():
 # Main code
 def main():
     printLogo()
+    time.sleep(0.1)
+    print('')
+    # Ask permision
+    print('\x1b[32m USE OF THIS SOFTWARE IS AT YOUR OWN RISK. THE DEVELOPER SHALL NOT BE LIABLE FOR ANY DAMAGES RESULTING FROM THE USE OF THIS SOFTWARE.')
+    if (input("Do you agree? [Y/n] >") in ['Y','y']):
+        time.sleep(0.1)
+        os.system('clear')
+        printLogo()
+        print('')
 
-
-
-
+        #### Start prigram
+        # Ask target BSSID
+        target_id = input("Enter target BSSID >")
+        if len(target_id) < 1:
+            print("[!] ERROR : Target BSSID error")
+            exit(0)
+        
+        # Ask dos ammount
+        dos_amt = input("default[10000] Enter DOS amount >")
+        if dos_amt == "":
+            dos_amt = 10000
+        ### test ###
+        elif len(dos_amt) < 1:
+            print("[!] ERROR : Dos amount error")
+            exit(0)
+        
 
 if __name__ == '__main__':
     try:
-        os.system('cls')
+        os.system('clear')
         main()
+    # Error doing something
     except KeyboardInterrupt:
         time.sleep(0.1)
         print('\n[*] Aborted')
         exit(0)
+    # Print error info
     except Exception as e:
         time.sleep(0.1)
         print('[!] ERROR: ' + str(e))
